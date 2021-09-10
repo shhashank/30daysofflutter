a39abd27cfe5f108b2e7e093a749b72f59636239
shhashank <shashanksherry@gmail.com>
2021-09-10 15:32:12 +0530

    day10  completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,26 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+      id: "Codepur001",
+      name: "iphone 12 pro",
+      desc: "Apple iphone 12 gen",
+      price: 999,
+      color: "#33505",
+      image: "http://wowdetailingstudio.com/asset/img/logo.png")
+];
